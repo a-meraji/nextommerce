f5eab3113eb47a7acb9433725ad9da4bb858fcd4
a-meraji <merajiamin1997@gmail.com>
2021-12-25 01:19:09 +0330

    infinite banner

--- components/Intro.js
+++ components/Intro.js
@@ -8,12 +8,57 @@ export default function Intro() {
       once: false,
       duration: 2500,
     });
+    // if(typeof window !== "undefined"){
+    //   window.onload(initSmoothScrolling('.block','smoothscroll'));
+    // }
   }, []);
+
+  // function initSmoothScrolling(container,animation){
+  //   /*
+  //    * @param {String} container Class or ID of the animation container
+  //    * @param {String} animation Name of the animation, e.g. smoothscroll
+  //  */
+  //    var sliderWidth = 0;
+  //    var animationWidth = 0;
+  //    var sliderHeight = ('>div>div:first-of-type',container).outerHeight(false);
+
+  //    $('>div>div', container).each(function(){
+  //      animationWidth += $(this).outerWidth(false);
+  //    });
+
+  //    // detect number of visible slides
+  //    var slidesVisible = $(container).width() / $('>div>div:first-of-type',container).outerWidth(false);
+  //    slidesVisible = Math.ceil(slidesVisible);
+
+  //    // count slides to determine animation speed
+  //    var slidesNumber = $('>div>div', container).length;
+  //    var speed = slidesNumber*2;
+
+  //  // append the tail
+  //    $('>div>div',container).slice(0,slidesVisible).clone().appendTo($('>div',container));
+
+  //    // Detect the slider width with appended tail
+  //    $('>div>div', container).each(function(){
+  //      sliderWidth += $(this).outerWidth(false);
+  //    });
+
+  //    // set slider dimensions
+  //    $('>div',container).css({'width':sliderWidth,'height':sliderHeight});
+
+  //  // Insert styles to html
+  //    $("<style type='text/css'>@keyframes "+animation+" { 0% { margin-left: 0px; } 100% { margin-left: -"+animationWidth+"px; } } "+$('>div>div:first-of-type',container).selector+" { -webkit-animation: "+animation+" "+speed+"s linear infinite; -moz-animation: "+animation+" "+speed+"s linear infinite; -ms-animation: "+animation+" "+speed+"s linear infinite; -o-animation: "+animation+" "+speed+"s linear infinite; animation: "+animation+" "+speed+"s linear infinite; }</style>").appendTo("head");
+
+  //    // restart the animation (e.g. for safari & ie)
+  //    var cl = $(container).attr("class");
+  //    $(container).removeClass(cl).animate({'nothing':null}, 1, function () {
+  //      $(this).addClass(cl);
+  //    });
+  //  }
 
   return (
     <>
       <div className="absolute top-0 left-0 right-0 text-secondary curier">
-        <div className="w-screen h-[100vh] bg-secondary">
+        <div className="h-[100vh] bg-secondary">
           <div className="relative h-screen left-0 right-0">
             <img
               data-aos="fade"
@@ -28,7 +73,7 @@ export default function Intro() {
             </p>
           </div>
         </div>
-        <div className="w-screen h-[50vh] bg-secondary">
+        <div className="h-[50vh] bg-secondary">
           <div className="relative h-[50vh] left-0 right-0">
             <p
               data-aos="fade"
@@ -38,26 +83,119 @@ export default function Intro() {
             </p>
           </div>
         </div>
-        <div className="bg-secondary">
-          <div className="relative  left-0 right-0 min-h-screen">
-          <img
-              data-aos="zoom-out-up"
-              src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F115%2F489%2FHat-front-black__72990.1603748583.png&w=640&q=85"
-              className="w-screen sm:w-[30%] object-contain sm:absolute sm:top-1/4 sm:left-0 bg-transparent"
-            />
-            <img
-              data-aos='zoom-out-up'
-              src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F118%2F507%2FSurgical_Mask_black.G01__86690.1602592629.png&w=640&q=85"
-              className="w-screen sm:w-[30%] object-contain sm:absolute  sm:top-[12%] sm:left-[35%] bg-transparent"
-            />
-            <img
-              data-aos='zoom-out-up'
-              src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F117%2F534%2FMen-TShirt-Black-Front__70046.1603748348.png&w=640&q=85"
-              className="w-screen sm:w-[30%]  object-contai sm:absolute  sm:top-1/4 sm:right-0  bg-transparent"
-            />
-          </div>
+        {/* banner */}
+
+        <div className="animation w-full whitespace-nowrap overflow-scroll scrollbar-hide">
+
+            <div className="first">
+              <img
+                className="w-full object-contain"
+                src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F115%2F489%2FHat-front-black__72990.1603748583.png&w=1920&q=85"
+              />
+            </div>
+            <div>
+              <img
+                className="w-full object-contain"
+                src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F116%2F512%2FMen-Jacket-Front-Black__15466.1603283963.png&w=1920&q=85"
+              />
+            </div>
+            <div>
+              <img
+                className="w-full object-contain"
+                src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F117%2F534%2FMen-TShirt-Black-Front__70046.1603748348.png&w=1920&q=85"
+              />
+            </div>
+            <div>
+              <img
+                className="w-full object-contain"
+                src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F118%2F508%2FSurgical-Mask-Black__89554.1603756821.png&w=750&q=85"
+              />
+            </div>
+            <div>
+              <img
+                className="w-full object-contain"
+                src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F122%2F412%2Fmockup-008b81ff__29393.1603748041.jpg&w=1920&q=85"
+              />
+            </div>
+            <div>
+              <img
+                className="w-full object-contain"
+                src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F126%2F423%2Fmockup-9c5e3634__60853.1603747807.png&w=1920&q=85"
+              />
+            </div>
+            <div>
+              <img
+                className="w-full object-contain"
+                src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F115%2F489%2FHat-front-black__72990.1603748583.png&w=1920&q=85"
+              />
+            </div>
+            <div>
+              <img
+                className="w-full object-contain"
+                src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F116%2F512%2FMen-Jacket-Front-Black__15466.1603283963.png&w=1920&q=85"
+              />
+            </div>
+            <div>
+              <img
+                className="w-full object-contain"
+                src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn11.bigcommerce.com%2Fs-qfzerv205w%2Fimages%2Fstencil%2Foriginal%2Fproducts%2F117%2F534%2FMen-TShirt-Black-Front__70046.1603748348.png&w=1920&q=85"
+              />
+            </div>
         </div>
+
+        {/* bann */}
       </div>
     </>
   );
 }
+
+//
+
+/* 
+$(window).on('load', function(){
+  // Please run it with window.onload, not with document.ready
+  initSmoothScrolling('.block','smoothscroll');
+});
+
+function initSmoothScrolling(container,animation){
+ /*
+	* @param {String} container Class or ID of the animation container
+	* @param {String} animation Name of the animation, e.g. smoothscroll
+
+	var sliderWidth = 0;	
+	var animationWidth = 0;	
+	var sliderHeight = $('>div>div:first-of-type',container).outerHeight(false);
+
+	$('>div>div', container).each(function(){				
+		animationWidth += $(this).outerWidth(false);		
+	});
+	
+	// detect number of visible slides
+	var slidesVisible = $(container).width() / $('>div>div:first-of-type',container).outerWidth(false);	
+	slidesVisible = Math.ceil(slidesVisible);
+
+  // count slides to determine animation speed
+	var slidesNumber = $('>div>div', container).length;
+	var speed = slidesNumber*2;
+	
+// append the tail	
+	$('>div>div',container).slice(0,slidesVisible).clone().appendTo($('>div',container));	
+
+	// Detect the slider width with appended tail
+	$('>div>div', container).each(function(){
+		sliderWidth += $(this).outerWidth(false);
+	});
+
+	// set slider dimensions
+	$('>div',container).css({'width':sliderWidth,'height':sliderHeight});
+  
+// Insert styles to html
+	$("<style type='text/css'>@keyframes "+animation+" { 0% { margin-left: 0px; } 100% { margin-left: -"+animationWidth+"px; } } "+$('>div>div:first-of-type',container).selector+" { -webkit-animation: "+animation+" "+speed+"s linear infinite; -moz-animation: "+animation+" "+speed+"s linear infinite; -ms-animation: "+animation+" "+speed+"s linear infinite; -o-animation: "+animation+" "+speed+"s linear infinite; animation: "+animation+" "+speed+"s linear infinite; }</style>").appendTo("head");	
+
+	// restart the animation (e.g. for safari & ie)	
+	var cl = $(container).attr("class");
+	$(container).removeClass(cl).animate({'nothing':null}, 1, function () {
+		$(this).addClass(cl);
+	});
+}
+*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -33,5 +33,7 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+  ],
 }
