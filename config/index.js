264de721496c004ebd2a77c7795af33d0c9ed62c
a-meraji <merajiamin1997@gmail.com>
2022-01-24 19:46:41 +0330

    deploy version 1.01

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV == 'production';
 
-export const server = dev ? 'http://localhost:3000' : '';
+export const server = dev ? 'http://localhost:3000' : 'nextommerce.vercel.app';
