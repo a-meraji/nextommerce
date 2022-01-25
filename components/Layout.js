3ea7c6371f9a37cd4b68a3c723f1a28de5127d53
a-meraji <merajiamin1997@gmail.com>
2022-01-25 10:17:17 +0330

    deploy version 1.03 loading screen fully  implemented

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
-import Cart from './cart/Cart'
-import Footer from './Footer'
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Loading from "./Loading";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Cart from "./cart/Cart";
+import Footer from "./Footer";
 
-export default function Layout({children}) {
-    return (
-        <div className='content glob-trans'>
-            <Navbar/>
-            <Sidebar />
-            <Cart />
-            {children}
-            <Footer/>
+export default function Layout({ children }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url) => setLoading(true);
+    const handleComplete = (url) => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
+  return (
+    <>
+        <div className="content glob-trans">
+        <Loading loading={loading}/>
+          <Navbar />
+          <Sidebar />
+          <Cart />
+          {children}
+          <Footer />
         </div>
-    )
+    </>
+  );
 }
---
+++ components/Loading.js
@@ -0,0 +1,25 @@
+import styles from "../shared/styles/loading.module.css";
+
+export default function Loading({loading}) {
+  return (
+    <div
+    style={{zIndex:loading?"100":"-100", display:loading?"block":"none"}}
+      className={styles.loading}
+    >
+        <div>
+
+      <svg width="205" height="250" viewBox="0 0 40 50">
+        <polygon
+          strokeWidth="1"
+          stroke="#fff"
+          fill="none"
+          points="20,1 40,40 1,40"
+          ></polygon>
+        <text fill="#fff" x="5" y="47">
+          Loading
+        </text>
+      </svg>
+          </div>
+    </div>
+  );
+}
