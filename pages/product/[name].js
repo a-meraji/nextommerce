3b9a5e8cbad2faf8d2deb55e6f277f129323d481
a-meraji <merajiamin1997@gmail.com>
2022-01-26 13:44:43 +0330

    deploy version 1.03 about page

--- pages/product/[name].js
+++ pages/product/[name].js
@@ -11,17 +11,21 @@ export default function id({ product, relateds }) {
         {/* single product section */}
        <SingleProduct product={product}/>
         {/* related section */}
-        <section className="border-t-[1px] border-b-[1px] border-gray-300">
+        <section className="border-t-[1px] border-gray-300">
           <h4 className="text-3xl text-primary text-center capitalize py-16">
             Related Products
           </h4>
           <div className="w-[85%] sm:w-[75%] mx-auto">
             <GridProducts products={relateds} limit="10" />
-          </div>
-          
-
-
+          </div>       
         </section>
+        <div className="w-full flex justify-center py-10">
+          <button className="py-2 px-5 bg-third text-primary rounded-full hover:scale-105 transition-transform">
+            <Link href="/search">
+              <a>All Products</a>
+            </Link>
+          </button>
+        </div>
       </div>
     </>
   );
