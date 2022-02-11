a568414e5289ee4aac35e58afc5dbb1418573e7a
a-meraji <merajiamin1997@gmail.com>
2022-02-11 12:53:36 +0330

    pwa2

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@ const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   pwa: {
-    dest: "bublic",
-    disable: process.env.NODE_ENV === 'development'
+    dest: "bublic"
   },
   images: {
     domains: ["demo.vercel.store"],
