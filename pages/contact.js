8e2da16ac4b1c00811035ded6812852524d8026b
a-meraji <merajiamin1997@gmail.com>
2022-02-12 13:10:46 +0330

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -5,7 +5,7 @@ function contact() {
     return (
         <div className="bg-secondary text-secondary py-14 px-5">
             <h2 className="text-primary text-lg font-bold">Contacts:</h2>
-            <ul>
+            <ul className="mb-10">
                 <li><a href="https://t.me/amin_mrj"><FaTelegram/> t.me/amin_mrj</a></li>
                 <li><FiAtSign/> email: merajiamin1997@gmail.com</li>
             </ul>
