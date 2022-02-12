04fa8b549a7dffb38cd162ba1046504af4cdaca7
a-meraji <merajiamin1997@gmail.com>
2022-02-12 13:10:28 +0330

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,14 @@
+import {FaTelegram} from "react-icons/fa";
+import {FiAtSign} from "react-icons/fi";
+
 function contact() {
     return (
-        <div>
-            contact
+        <div className="bg-secondary text-secondary py-14 px-5">
+            <h2 className="text-primary text-lg font-bold">Contacts:</h2>
+            <ul>
+                <li><a href="https://t.me/amin_mrj"><FaTelegram/> t.me/amin_mrj</a></li>
+                <li><FiAtSign/> email: merajiamin1997@gmail.com</li>
+            </ul>
         </div>
     )
 }
